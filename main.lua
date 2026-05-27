c56e09363657c3f692ddd4397685f7cb0232d5cc
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-27 17:49:46 +0300

    fix: ball initial position

--- main.lua
+++ main.lua
@@ -16,9 +16,11 @@ State = {
 }
 push = require("push")
 function love.load()
+	love.window.setTitle("Pong")
 	love.graphics.setDefaultFilter("nearest", "nearest")
-	font = love.graphics.newFont("font.ttf", 32)
-	love.graphics.setFont(font)
+	largeFont = love.graphics.newFont("font.ttf", 32)
+	smallFont = love.graphics.newFont("font.ttf", 8)
+	love.graphics.setFont(largeFont)
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { resizable = false, vsync = false, fullscreen = false })
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
 
@@ -27,7 +29,7 @@ function love.load()
 
 	player1 = Paddle(10, 10, 5, 20)
 	player2 = Paddle(VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
-	ball = Ball(HVW - 2, HVW - 2, 4, 4)
+	ball = Ball(HVW - 2, HVH - 2, 4, 4)
 end
 function love.update(dt)
 	if love.keyboard.isDown("w") then
@@ -66,10 +68,14 @@ end
 function love.draw()
 	push:start()
 	love.graphics.clear(0.1569, 0.176, 0.204, 1)
+	love.graphics.setFont(largeFont)
 	player1:render()
 	player2:render()
 	ball:render()
 	love.graphics.print(tostring(player1.score), HVW - 50, HVH - 80)
 	love.graphics.print(tostring(player2.score), HVW + 30, HVH - 80)
+
 	push:finish()
 end
+
+
