83f5b3d4e48b8a21a8cff858a079a96d835341b6
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-27 19:23:13 +0300

    fix: clamping ball.y when hitting vertical edge

--- Ball.lua
+++ Ball.lua
@@ -12,7 +12,11 @@ end
 function Ball:update(dt)
 	self.x = self.x + self.dx * dt
 	self.y = self.y + self.dy * dt
-	if self.y >= VIRTUAL_HEIGHT - self.height or self.y <= 0 then
+	if self.y >= VIRTUAL_HEIGHT - self.height then
+		self.y = VIRTUAL_HEIGHT - self.height
+		self.dy = -self.dy
+    elseif self.y <= 0 then
+		self.y = 0
 		self.dy = -self.dy
 	end
 end
