3cfeb0abff9eec7759e94df11f1593f625bda6c2
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-27 16:14:56 +0300

    feat: implement movement of paddles with clamping

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 HVW = VIRTUAL_WIDTH / 2
 HVH = VIRTUAL_HEIGHT / 2
+PADDLE_SPEED = 200
 push = require("push")
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
@@ -14,20 +15,34 @@ function love.load()
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
 
 	p1Score = 0
+	p1y = 10
 	p2Score = 0
+	p2y = VIRTUAL_HEIGHT - 30
 end
 function love.keypressed(key)
 	if key == "escape" then
 		love.event.quit()
 	end
 end
+function love.update(dt)
+	if love.keyboard.isDown("w") then
+		p1y = math.max(p1y - PADDLE_SPEED * dt, 0)
+	elseif love.keyboard.isDown("s") then
+		p1y = math.min(p1y + PADDLE_SPEED * dt, VIRTUAL_HEIGHT - 20)
+	end
+	if love.keyboard.isDown("up") then
+		p2y = math.max(p2y - PADDLE_SPEED * dt, 0)
+	elseif love.keyboard.isDown("down") then
+		p2y = math.min(p2y + PADDLE_SPEED * dt, VIRTUAL_HEIGHT - 20)
+	end
+end
 function love.draw()
 	push:start()
 	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 1)
 	-- paddle 1
-	love.graphics.rectangle("fill", 10, 10, 5, 20)
+	love.graphics.rectangle("fill", 10, p1y, 5, 20)
 	-- paddle 2
-	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+	love.graphics.rectangle("fill", VIRTUAL_WIDTH - 15, p2y, 5, 20)
 	-- ball
 	love.graphics.rectangle("fill", HVW - 2, HVH - 2, 4, 4)
 	-- p1 score
