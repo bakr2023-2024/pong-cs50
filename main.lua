d58061a7e12749572c5f7493a2499607470a69de
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-27 15:15:14 +0300

    feat: use retro font

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-VIRTUAL_WIDTH = 423
+VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 push = require("push")
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
+	font = love.graphics.newFont("font.ttf", 32)
+	love.graphics.setFont(font)
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { resizable = false, vsync = false, fullscreen = false })
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT)
 end
@@ -16,6 +18,7 @@ function love.keypressed(key)
 end
 function love.draw()
 	push:start()
-	love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, "center")
+	love.graphics.clear(45 / 255, 50 / 255, 20 / 255, 1)
+	love.graphics.printf("Hello Pong!", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, "center")
 	push:finish()
 end
